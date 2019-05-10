6c2031c5584a8bdb12fa41fe62fae901c6df111a
Administrator <zr2013123@sina.com>
2019-05-10 11:59:38 +0800

    update file

--- vlayout/src/main/java/com/alibaba/android/vlayout/customadapter/LoadInter.java
+++ vlayout/src/main/java/com/alibaba/android/vlayout/customadapter/LoadInter.java
@@ -4,8 +4,8 @@ package com.alibaba.android.vlayout.customadapter;
  *   created by android on 2019/4/29
  */
 public interface LoadInter {
-    int load = 10000;
-    int noMore = 10001;
-    int error = 10002;
+    int load = 8000;
+    int noMore = 8001;
+    int error = 8002;
     void result(int status);
 }
